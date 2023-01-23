cc7ffc26965d4a06230cda9a05eb02e6491b31a8
JudahSan <baraka.judah@yahoo.com>
2023-01-23 18:41:36 +0300

    Changing passwords

--- chatproject/users/urls.py
+++ chatproject/users/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('login/', views.user_login, name='login'),
     # path('login/', auth_view.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/',auth_view.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('password_change/', auth_view.PasswordChangeView.as_view(template_name='users/password_change_form.html'), name='password_change'),
+    path('password_change/done/', auth_view.PasswordChangeDoneView.as_view(template_name='users/password_change_done.html'), name='password_change_done'),
 ]
