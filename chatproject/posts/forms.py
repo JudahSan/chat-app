2192e81bd6f8e211edcccd816cc906c578a1b153
JudahSan <baraka.judah@yahoo.com>
2023-02-16 01:44:43 +0300

    Chat app styling

--- chatproject/posts/forms.py
+++ chatproject/posts/forms.py
@@ -1,8 +1,16 @@
 from .models import Post
 from django import forms 
 from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit 
 
 class PostCreateForm(forms.ModelForm):
+
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+    self.helper = FormHelper(self)
+    self.helper.add_input(Submit('submit', 'Submit'))
+
+
   class Meta:
     model = Post 
     fields =  ('title', 'image', 'caption')
